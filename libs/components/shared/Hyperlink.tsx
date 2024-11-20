d01c762951c1e2e95616ea0cdf6eee940007480a
RichClayroadCR <rich@clayroadproductions.com>
2024-11-20 21:48:02 +0000

    fix: Replace A Tag with NextJS Links

--- libs/components/shared/Hyperlink.tsx
+++ libs/components/shared/Hyperlink.tsx
@@ -1,12 +1,19 @@
 import classNames from "@sindresorhus/class-names";
+import Link from "next/link";
 import type { AnchorHTMLAttributes } from "react";
 
 interface HyperlinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {}
 
-export function Hyperlink({ className, children, ...props }: HyperlinkProps) {
-	return (
-		<a {...props} rel="noopener noreferrer" className={classNames(className, "cursor-pointer")}>
+export function Hyperlink({ className, children, href, ...props }: HyperlinkProps) {
+	
+	return (href?.startsWith("http") || href?.startsWith("mailto:")) ? (
+		<a {...props} href={href ?? ""} rel="noopener noreferrer" className={classNames(className, "cursor-pointer")}>
 			{children}
 		</a>
-	);
+	) : (
+		<Link href={href ?? ""} {...props} rel="noopener noreferrer" className={classNames(className, "cursor-pointer")}>
+			{children}
+		</Link>
+	) 
+
 }
