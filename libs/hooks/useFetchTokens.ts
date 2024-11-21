b2aebb9624221f4627f7c761add9d80029a978c0
RichClayroadCR <rich@clayroadproductions.com>
2024-11-21 00:13:52 +0000

    fix cache for token

--- libs/hooks/useFetchTokens.ts
+++ libs/hooks/useFetchTokens.ts
@@ -6,7 +6,9 @@ export function useFetchTrnTokens(trnTokens?: TrnTokens) {
 	return useQuery({
 		queryKey: ["tokenMetadata"], 
 		queryFn: async () => await fetchTrnTokens(),
-		staleTime: Infinity,
+		staleTime: 1000 * 60,
 		initialData: trnTokens,
+		refetchInterval: 1000 * 60 * 5,
+		refetchOnWindowFocus: true,
 	})
 }
