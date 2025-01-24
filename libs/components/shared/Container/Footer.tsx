f950d29a4c08bf3a317bdc21c84e4d0c1ce734fd
Nick95550 <Nicholas@sylo.io>
2025-01-24 13:36:32 +1300

    update package version

--- libs/components/shared/Container/Footer.tsx
+++ libs/components/shared/Container/Footer.tsx
@@ -25,7 +25,7 @@ export function Footer() {
 				</Hyperlink>
 			</span>
 
-			<Text>Version 1.2.1</Text>
+			<Text>Version 1.2.2</Text>
 
 			<span>&copy; Dexter {new Date().getFullYear()}</span>
 		</footer>
