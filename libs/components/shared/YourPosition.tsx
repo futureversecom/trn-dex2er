913b0a63c5c76159f99f13b7b80aca817881e52a
Nick95550 <ngjvarley@gmail.com>
2025-03-13 11:18:13 +1300

    fix: add explicit element colors

--- libs/components/shared/YourPosition.tsx
+++ libs/components/shared/YourPosition.tsx
@@ -16,29 +16,30 @@ export function YourPosition() {
 	);
 
 	return (
-		<div className="h-[16em] w-[25em] rounded-2xl bg-neutral-200 p-8">
+		<div className="h-[16em] w-full max-w-[30em] rounded-2xl bg-neutral-200 p-8">
 			<div className="grid grid-cols-2 gap-4">
-				<p className="font-mikrobe col-span-2 text-center">Your Position</p>
+				<p className="font-mikrobe col-span-2 text-center text-neutral-700">Your Position</p>
 
-				<div className="flex items-center">
+				<div className="flex items-center gap-2">
 					<Tokens xSymbol={props.xToken?.symbol} ySymbol={props.yToken?.symbol} />
-					<Text>{`${props.xToken?.symbol}/${props.yToken?.symbol}`}</Text>
+					<Text className="ml-1 truncate">{`${props.xToken?.symbol}/${props.yToken?.symbol}`}</Text>
 				</div>
-
-				<p className="break-words text-right">{currentPosition?.lpBalance.toHuman()}</p>
+				<p className="break-words text-right text-neutral-700">
+					{currentPosition?.lpBalance.toHuman()}
+				</p>
 
 				<Text>Your pool share</Text>
-				<p className="text-right">
+				<p className="text-right text-neutral-700">
 					{currentPosition && toFixed(currentPosition?.poolShare.toNumber(), 3)}%
 				</p>
 
 				<Text>{xSymbol}</Text>
-				<p className="text-right">
+				<p className="text-right text-neutral-700">
 					{props.poolBalances && toFixed(props.poolBalances.x.balance.toUnit().toNumber(), 4)}
 				</p>
 
 				<Text>{ySymbol}</Text>
-				<p className="text-right">
+				<p className="text-right text-neutral-700">
 					{props.poolBalances && toFixed(props.poolBalances?.y.balance.toUnit().toNumber(), 4)}
 				</p>
 			</div>
